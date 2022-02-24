ea2b1895c2278e98c1209f6cecf7476791429c1b
NItish Chandra <nitishchandra1109@gmail.com>
2022-02-24 19:34:42 +0530

    First Commit

---
+++ auto_watsapp.py
@@ -0,0 +1,14 @@
+import  pywhatkit
+
+pywhatkit.sendwhatmsg("+918763863743","hello hassan",1,52)
+
+
+
+
+# first install pip install flask
+# then install pip install pywhatkit
+# then this will work...!!!!!!!!!
+
+
+# THANKS
+# NITISH CHANDRA
